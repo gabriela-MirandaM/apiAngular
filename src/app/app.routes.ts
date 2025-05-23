70ea9a8115098c183d6cfbcac11078e050501009
Gabriela Miranda <mm15136@ues.edu.sv>
2025-05-23 09:15:45 -0600

    Pantalla de inicio de sesion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { AuthLayoutComponent } from './layout/auth-layout/auth-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:"",
+    component:MainLayoutComponent
+  },
+  {
+    path:"auth",
+    component:AuthLayoutComponent
+  }
+];
---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.component.html',
+})
+export class HeaderComponent {
+
+  toggleTheme(){
+    const html = document.documentElement;
+    html.setAttribute('data-theme', html.getAttribute('data-theme')=== 'light'?'dark':'light');
+  }
+  logout(){
+    console.log('Cerrando sesion...');
+  }
+}
